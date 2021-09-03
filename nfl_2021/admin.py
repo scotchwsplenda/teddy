6ce8f420bc735826b70627b57feb6b6f69fca8ec
scotchwsplenda <lockwood.oliver@gmail.com>
2021-09-03 11:18:05 -0700

    registered nfl teams in admin file for use on admin page through superuser

--- nfl_2021/admin.py
+++ nfl_2021/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
-from .models import predicted_score
+from .models import predicted_score, nfl_teams
 # Register your models here.
 admin.site.register(predicted_score)
+admin.site.register(nfl_teams)
