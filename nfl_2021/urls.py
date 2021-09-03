3060cc996f96375907aabf61b277f93c7b8c8f5d
scotchwsplenda <lockwood.oliver@gmail.com>
2021-09-03 10:04:05 -0700

    trying only home url

--- nfl_2021/urls.py
+++ nfl_2021/urls.py
@@ -3,11 +3,11 @@ from django.views.generic import TemplateView
 from . import views
 
 urlpatterns = [
-    path('cool_form/', views.choose_team_view),
-    path('cool_form/<acr>/', views.cool_form_view, name='cool_form_team'),
+    # path('cool_form/', views.choose_team_view),
+    # path('cool_form/<acr>/', views.cool_form_view, name='cool_form_team'),
     path('', TemplateView.as_view(template_name='nfl_2021/home.html')),
-    path('results/', views.choose_results_view),
-    path('results/<acr>', views.results_view, name='results_team'),
-    path('accuracy/', TemplateView.as_view(template_name='nfl_2021/accuracy.html')),
+    # path('results/', views.choose_results_view),
+    # path('results/<acr>', views.results_view, name='results_team'),
+    # path('accuracy/', TemplateView.as_view(template_name='nfl_2021/accuracy.html')),
 
 ]
